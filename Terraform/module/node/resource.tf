8de6c9ced3ae9ed0bb7b60d64114db91b3d20a5e
aida <ghk01214@naver.com>
2024-11-26 13:42:38 +0900

    Auto stash before merge of "main" and "origin/main"

--- Terraform/module/node/resource.tf
+++ Terraform/module/node/resource.tf
@@ -3,11 +3,11 @@ resource "aws_instance" "main" {
   instance_type          = "t2.micro"
   subnet_id              = var.private_subnet_id
   vpc_security_group_ids = [var.security_group_id]
-  private_ip             = "10.0.3.5"
+  private_ip             = "10.0.2.5"
   iam_instance_profile   = "ec2-ssm"
   
   tags = {
-    Name = "KTB SSM",
+    Name = "KTB SSM Public",
     Environment = "SSM"
   }
 }
